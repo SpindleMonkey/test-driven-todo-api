474ab1505cb2fe12015be334fe2b51bc1d550627
connie kephart <spindle.monkey@gmail.com>
2017-07-24 16:15:01 -0600

    implements show, create, update and delete

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ var todos = [
   { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
+var newTodo = {
+  _id: 0,
+  task: '',
+  description: '',
+};
+
 /**********
  * ROUTES *
  **********/
@@ -48,6 +54,7 @@ app.get('/api/todos/search', function search(req, res) {
   /* This endpoint responds with the search results from the
    * query in the request. COMPLETE THIS ENDPOINT LAST.
    */
+  console.log(req.body);
 });
 
 app.get('/api/todos', function index(req, res) {
@@ -55,29 +62,60 @@ app.get('/api/todos', function index(req, res) {
 });
 
 app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
+  newTodo._id = todos.length + 1;
+  newTodo.task = req.body.task;
+  newTodo.description = req.body.description;
+  todos.push(newTodo);
+  res.json(newTodo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
+  var id = req.params.id;
+  //console.log('id: ' + id);
+  for (var i = 0; i < todos.length; i++) {
+    //console.log('i; ' + i);
+    if (todos[i]._id == id) {
+      res.send(todos[i]);
+      break;
+    }
+  }
+  res.send('ERROR::not found');
 });
 
 app.put('/api/todos/:id', function update(req, res) {
-  /* This endpoint will update a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with the newly updated todo.
-   */
+ console.log(req.body);
+  for (var k = 0; k < todos.length; k++) {
+    if (todos[k]._id == req.params.id) {
+      if (req.body.task) {
+        todos[k].task = req.body.task;
+      }
+      if (req.body.description) {
+        todos[k].description = req.body.description;
+      }
+      res.json(todos[k]);
+    }
+  }
+  res.send('ERROR::' + req.params.id + ' not found');
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
-  /* This endpoint will delete a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with deleted todo.
-   */
+  for (var j = 0; j < todos.length; j++) {
+    if (todos[j]._id == req.params.id) {
+      // save a copy of the deleted todo
+      // newTodo._id = todos[j]._id;
+      // newTodo.task = todos[j].task;
+      // newTodo.description = todos[j].description;
+
+      // todos[j]._id= -1;
+      // todos[j].task = '';
+      // todos[j].description = '';
+      //res.json(newTodo);
+      res.json(todos[j]);
+      todos.splice(j, 1);
+
+      //res.json(newTodo);
+    }
+  }
 });
 
 /**********
